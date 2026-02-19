6ee70f048796fefa409febbad3df24ae2932fc9b
YangChiayin <joy851026@gmail.com>
2026-02-19 13:17:59 +0800

    url updated

--- server/server.ts
+++ server/server.ts
@@ -21,7 +21,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000', "https://thumblify-ed1r.vercel.app/"],
+    origin: ['http://localhost:5173', 'http://localhost:3000', "https://thumblify-weld.vercel.app/login"],
     credentials: true
 }))
 
